cc9b719bf458e7a8749b4e606c9b959f3d3c3cd9
metawin99 <pai.pp61@gmail.com>
2018-08-06 17:27:54 +0700

    Initial Commit

---
+++ IndicatorSteper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndicatorSteper
+//
+//  Created by Soemsak on 6/8/2561 BE.
+//  Copyright © 2561 Soemsak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
