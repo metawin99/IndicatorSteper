b410937786964bf3bfe462579ffc761489ba1375
metawin99 <pai.pp61@gmail.com>
2018-08-07 11:25:53 +0700

    first commit

---
+++ IndicatorSteper/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  IndicatorSteper
+//
+//  Created by Soemsak on 7/8/2561 BE.
+//  Copyright © 2561 Soemsak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IndicatorSteper/SwiftStepView/StepView.swift
@@ -0,0 +1,9 @@
+//
+//  StepView.swift
+//  IndicatorSteper
+//
+//  Created by Soemsak on 7/8/2561 BE.
+//  Copyright © 2561 Soemsak. All rights reserved.
+//
+
+import Foundation
